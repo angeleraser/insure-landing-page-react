e3aa69f1b34ca9ad24401ca55b753a36a48a7037
angeleraser <angeleraser@gmail.com>
2020-07-21 23:41:25 -0400

    Add build folder

---
+++ build/precache-manifest.5c3c15a049da16fe7636f19c7ef10380.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625cfc5652af2d2e0e14d76408e06738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e9c0233ffcef32e104d",
+    "url": "/static/css/main.8ac4fd60.chunk.css"
+  },
+  {
+    "revision": "36729dc454591070a153",
+    "url": "/static/js/2.85a878ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85a878ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9c0233ffcef32e104d",
+    "url": "/static/js/main.3d130fcb.chunk.js"
+  },
+  {
+    "revision": "3400052dfee0438bc738",
+    "url": "/static/js/runtime-main.06fe0fd1.js"
+  },
+  {
+    "revision": "4abb74643a40d67264d0e192655faa22",
+    "url": "/static/media/bg-pattern-how-we-work-desktop.4abb7464.svg"
+  },
+  {
+    "revision": "a4de14b0ee74ee9f31076b875f4ef78e",
+    "url": "/static/media/bg-pattern-how-we-work-mobile.a4de14b0.svg"
+  },
+  {
+    "revision": "d57cc9d5ede81487205ac0071030bb57",
+    "url": "/static/media/icon-close.d57cc9d5.svg"
+  },
+  {
+    "revision": "d98c1990131df21b2dc25cd8c1382819",
+    "url": "/static/media/icon-hamburger.d98c1990.svg"
+  },
+  {
+    "revision": "8ac69ec7c30fa6465fa05426a4b235f4",
+    "url": "/static/media/image-intro-desktop.8ac69ec7.jpg"
+  }
+]);
